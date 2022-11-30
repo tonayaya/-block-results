63603bed78f8297076c409c54a10151568230eef
tonayaya <tonayaya@icloud.com>
2022-11-30 19:53:41 +0400

    измененное решение с описанием

--- resultEx/Program.cs
+++ resultEx/Program.cs
@@ -3,32 +3,46 @@
 // длина которых меньше, либо равна 3 символам. 
 
 
-string[] source = { "единицы", "десятки", "сотни", "тысячи", "1", ":-)", "New York", "-2", "Windows" };
+// Объявление исходного массива
+string[] source = { "Russian", "Kazan", "DD", "HI", "1", ":-)", "New York", "-2", "Windows" };
+
+// Объявление методов и функций
+// Вывод массива
 void PrintArray(string[] m)
 {
-for (int x = 0; x < m.Length; x++)
-Console.Write($"\"{m[x]}\" ");
-Console.WriteLine();
+    for (int x = 0; x < m.Length; x++)
+    Console.Write($"\"{m[x]}\" ");
+    Console.WriteLine();
 }
 string[] copyStringsBelowOrEqual3InNewArray(string[] strArray)
 {
-int k = 0;
-for (int i = 0; i < strArray.Length; i++)
-if (strArray[i].Length <= 3) k++;
-string[] newArray = new string[k];
-k = 0;
-for (int i = 0; i < strArray.Length; i++)
-if (strArray[i].Length <= 3) newArray[k++]=strArray[i];
-return newArray;
+    // Определяем количество строк, удовлетворяющих условию
+    int k = 0;
+    for (int i = 0; i < strArray.Length; i++)
+    if (strArray[i].Length <= 3) k++;
+
+    // Создаем массив для результата
+    string[] newArray = new string[k];
+
+    // Копирование в результирующий массив строк, удовлетворяющих условию
+    k = 0;
+    for (int i = 0; i < strArray.Length; i++)
+    if (strArray[i].Length <= 3) newArray[k++]=strArray[i];
+
+    return newArray;
 }
 
-
+// Начало программы
 Console.WriteLine("Программа создания нового массива и копирования в него строк длиной <=3");
+
+// Вывод исходного массива
 Console.WriteLine("Исходный массив строк:");
 PrintArray(source);
 
+// Создать новый массив с результатом
 string[] target = copyStringsBelowOrEqual3InNewArray(source);
 
+// Показать результат
 Console.WriteLine("Результирующий массив строк:");
 PrintArray(target);
 Console.WriteLine();
